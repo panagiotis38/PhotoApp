4e0270d5a4bef05a1fe9af9d4bf920a2f1b2559a
panagiotis38 <64253326+panagiotis38@users.noreply.github.com>
2025-08-03 16:53:21 +0300

    changed url and key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY'; 
+const SUPABASE_URL = 'https://zxulxhafnynzxggnquqo.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4dWx4aGFmbnluenhnZ25xdXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMjU0NTYsImV4cCI6MjA2OTgwMTQ1Nn0.W-YPy7aFktt1KHzJX22DfRzdj_LW2FDYWjs4IvFp0PQ'; 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const video = document.getElementById('preview');
